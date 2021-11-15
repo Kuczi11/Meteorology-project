c4e18e2f9c39627247067bd7c9c9731a04453df0
Lukasz Kuczynski <lukaszkuczynski@bpol.net>
2021-11-15 10:18:01 +0100

    compose store with redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,15 @@
-import { createStore } from '@reduxjs/toolkit';
+import { createStore, compose } from '@reduxjs/toolkit';
+
+let composeEnhancers = compose;
+
+if (process.env.NODE_ENV === 'development') {
+  // Compose with redux devtools if it exist in the browser
+  const {
+    // @ts-ignore
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: devToolsCompose = null,
+  } = window;
+  composeEnhancers = devToolsCompose || compose;
+}
 
 const store = createStore(
 );
